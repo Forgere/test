ec2ff5a1ca39eadd52ea60ed7b46a271eca797fd
forgere <573596314@qq.com>
2016-03-01 22:51:45 +0800

    change js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,12 @@ $(function(){
 		$('.sound').toggleClass('off');
 			 // Stuff to do every *odd* time the element is clicked
 		})
+//page1 page2 触摸无效
+//滑动事件时 （不同方向）会使页面offset。top值变化
+//分lock lockup lockdown的情况
+//page的offset.top=0时，page的display＝block
 
+//page2点击就跳入page3 但是不同内容随点击的图片不同加入appendto，且根据加入的数量生成page3 456
 
 
 
